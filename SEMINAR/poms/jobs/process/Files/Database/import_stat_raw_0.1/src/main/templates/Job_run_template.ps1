bbff9ab747770b3f25c626ba803f45889e6e16a8
Cubiccl | Clément Chaussade <Cubiccl@users.noreply.github.com>
2018-12-21 14:17:35 +0100

    Job: import json into stat_raw table

---
+++ SEMINAR/poms/jobs/process/Files/Database/import_stat_raw_0.1/src/main/templates/Job_run_template.ps1
@@ -0,0 +1,3 @@
+$fileDir = Split-Path -Parent $MyInvocation.MyCommand.Path
+cd $fileDir
+java '-Dtalend.component.manager.m2.repository=%cd%/../lib' '-Xms256M' '-Xmx1024M' '-Dfile.encoding=UTF-8' -cp '.;../lib/routines.jar;../lib/accessors-smart-1.1.jar;../lib/dom4j-1.6.1.jar;../lib/jakarta-oro-2.0.8.jar;../lib/json-path-2.1.0.jar;../lib/json-smart-2.2.1.jar;../lib/jtds-1.3.1-patch.jar;../lib/log4j-1.2.17.jar;../lib/slf4j-api-1.7.5.jar;../lib/slf4j-log4j12-1.7.5.jar;import_stat_raw_0_1.jar;' seminar.import_stat_raw_0_1.import_stat_raw  %*
